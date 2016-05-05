1d4ec9237f8fd201c220d498cca07d359b2e37c4
TarikFojnica <tarik.f786@gmail.com>
2016-05-05 15:36:36 +0200

    add the slider feature

--- app/components/filter-form/filterFormController.js
+++ app/components/filter-form/filterFormController.js
@@ -1,3 +1,13 @@
 app.controller ('filterFormController', function($scope){
 
+	$scope.slider = {};
+
+	$scope.slider.budget = {
+		min: 30,
+		max: 50,
+		options: {
+			floor: 1,
+			ceil: 100
+		}
+	};
 });
